4d0a0d2c7f8d2f23c884967dc8ba8971ed20123f
ihnatchuk <ihnatchukpp@gmail.com>
2022-10-31 22:24:21 +0200

    JS hw1, task1

---
+++ hw1/script/main.js
@@ -0,0 +1,36 @@
+// first part
+let s1='hello';
+console.log(s1);
+alert(s1);
+document.writeln(s1);
+
+let s2='owu';
+let s3='com';
+let s4='ua';
+document.write(`${s2}.${s3}.${s4}`);
+console.log(s2+"."+s3+"."+s4);
+
+let num1=1;
+let num2=10;
+let num3=-999;
+let num4=123;
+document.write(num1, num2, num3, num4);
+console.log(num1, num2, num3, num4);
+
+const PI=3.14;
+const E=2.7;
+document.write(PI, E);
+console.log(PI, E);
+
+let num7=16;
+document.write(num7);
+console.log(num7);
+alert(num7);
+
+let bool1=true;
+let bool2=false;
+document.write(bool1, bool2);
+console.log(bool1, bool2);
+alert(bool1);
+// Чому document.write виконується після останнього alert()?
+
