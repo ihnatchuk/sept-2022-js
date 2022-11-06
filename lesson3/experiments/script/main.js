7d7b80ee26728211b08ec5d7c1394e2e09280dac
ihnatchuk <ihnatchukpp@gmail.com>
2022-11-07 01:05:42 +0200

    if, switch

---
+++ lesson3/experiments/script/main.js
@@ -0,0 +1,55 @@
+// let color=prompt('input color:');
+// if (color==='green' && confirm('is road clear?')){
+//     console.log("Go!")
+// }
+
+
+// let color=prompt('input color:');
+//
+// switch (color){
+//     case 'green':
+//         console.log('Go!');
+//         break;
+//     case 'yellow':
+//     case 'red':
+//         console.log('STOP');
+//         break;
+//     default:
+//         console.log('??????')
+// }
+// let bool;
+//
+// console.log(bool, typeof bool);
+// switch (bool){
+//     case null:
+//         console.log('null');
+//         break;
+//     case undefined:
+//         console.log('undefined');
+//         break;
+//     case NaN:
+//         console.log('NaN');
+//         break;
+//     default:
+//         console.log('??????')
+// }
+let num=0
+switch (true){
+    case num<=-20:
+        console.log('<=-20');
+        break;
+    case num<=-10:
+        console.log('-20<num<=-10');
+        break;
+    case num<=0:
+        console.log('-10<num<=0');
+        break;
+    case num<=10:
+        console.log('0<num<=10');
+        break;
+    case num<=20:
+        console.log('10<num<=20');
+        break;
+    default:
+        console.log('20<num')
+}
